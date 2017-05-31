30eaa90918acde73d5f418bebea57ea1645c6ba5
Earl Sabal <email": null>
2017-05-31 14:55:06 -0700

    Add content to methods within reviews controller-- new, create and destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,21 @@
 def new
-
+  @review = Review.new
 end
 
 def create
+  @review = Review.new(review_params)
 
+  if @review.save
+    redirect_to @review.movie
+  else
+    render 'new'
+  end
 end
 
 def destroy
+  @review = Review.find(params[:id])
+  @movie = @review.movie
+  @review.destroy
 
+  redirect_to @movie
 end
