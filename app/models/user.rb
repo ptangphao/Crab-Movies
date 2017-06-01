9eeaeda7a413b4511d07cbdaf773db30c4df2957
Earl Sabal <email": null>
2017-06-01 16:26:50 -0700

    add template for email, email working on registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :send_user_mail
+  def send_user_mail
+    UserMailer.send_enabled_message(self).deliver
+  end
 end
