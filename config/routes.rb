f62d41c7c575555d176010d74b93578ace311fe6
Patrick Tangphao <email": null>
2017-06-01 11:09:59 -0700

    update routes for actors to remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :movies, only: [:show]
   resources :categories, only: [:index, :show]
-  resources :actors
+  resources :actors, only: [:index, :show]
   root to:"categories#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
