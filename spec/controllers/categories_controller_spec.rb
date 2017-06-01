b7fb1c88cd748eb54317dab205d63403912763e4
Patrick Tangphao <email": null>
2017-06-01 11:37:38 -0700

    write spec for testing http status code for categories#show, pass rspec

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -9,6 +9,23 @@ describe CategoriesController do
       expect(response).to have_http_status 200
     end
 
+    it "assigns all categories as @categories" do
+      get :index
+      expect(assigns(:categories)).to eq(Category.all)
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "get #show" do
+    it "responds with a status code of 200" do
+      get :show, {id: category.id}
+      expect(response).to have_http_status 200
+    end
+
 
   end
 end
