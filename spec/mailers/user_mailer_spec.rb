684e7cbe7457b5b453e8eacf2b1333b4b5ab3fd9
Earl Sabal <email": null>
2017-06-01 16:59:26 -0700

    creat test for recipient email from mailer and pass rspec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe UserMailer, type: :mailer do
   let(:user){ User.new({email: 'earlsabal@gmail.com'})}
   let(:mail){UserMailer.send_enabled_message(user)}
   it "renders the subject" do
-    expect(mail.subject).to eql ('Welcome to Crab Movies!!!')
+    expect(mail.subject).to eq 'Welcome to Crab Movies!!!'
+  end
+
+  it "renders the correct recipient email" do
+    expect(mail.to[0]).to eq "earlsabal@gmail.com"
   end
 end
