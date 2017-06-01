394b8dcfc3344e620bf96ecdda77393224a016e5
Patrick Tangphao <email": null>
2017-06-01 11:43:39 -0700

    create test for http status code for movie#show, pass rspec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -9,5 +9,12 @@ describe MoviesController do
               poster_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
               category_id: 1
               })}
+  describe "get #show" do
+    it "responds with a status code of 200" do
+      get :show, {id: movie.id}
+      expect(response).to have_http_status 200
+    end
 
+
+  end
 end
