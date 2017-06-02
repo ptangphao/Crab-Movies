868b1dc8adc8dfd512bd73c971903832b5320161
Earl Sabal <email": null>
2017-06-01 17:45:49 -0700

    Refactor movies show page creating partials for actors and rating buttons, and create logic for showing average rating inside application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def rating(object)
+    total = object.ratings.inject(0) {|sum, rating| sum += rating.rating}
+    total_stars = total / object.ratings.length
+    stars = ""
+    total_stars.times {stars += "☆"}
+    stars
+  end
 end
