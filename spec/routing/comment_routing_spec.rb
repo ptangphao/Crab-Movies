275d5b3a8cbdcb9c17a02c4e5e83dde7710ee36b
Earl Sabal <email": null>
2017-06-01 17:30:26 -0700

    create rspec file for routing to comments

--- spec/routing/comment_routing_spec.rb
+++ spec/routing/comment_routing_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Routing to actors", :type => :routing do
-  it "routes GET /actors to actors#index" do
-    expect(:get => "/actors").to route_to('actors#index')
+RSpec.describe "Routing to comments", :type => :routing do
+  it "routes POST /comments to comments#create" do
+
   end
 
-  it "routes GET /actors/1 to actors#show" do
-    expect(:get => "/actors/1").to route_to('actors#show', :id => "1")
-  end
+  it "routes DELETE /comments/1 to comments#destroy"
 end
