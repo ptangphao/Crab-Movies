16c76edbfa9dbe039a860c159ae74d4bd331eb46
Earl Sabal <email": null>
2017-06-01 17:21:26 -0700

    update route for comments to only include create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       resources :ratings
     end
   end
-  resources :comments
+  resources :comments, only: [:create, :destroy]
   root to:"categories#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
