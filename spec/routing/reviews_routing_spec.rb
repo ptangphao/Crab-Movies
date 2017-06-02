a7d68cebc6c645a33eb85c695b6e3ebecf99f6ac
Patrick Tangphao <email": null>
2017-06-02 11:34:00 -0700

    update test for review destroy route

--- spec/routing/reviews_routing_spec.rb
+++ spec/routing/reviews_routing_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Routing to reviews", :type => :routing do
-   it "routes POST movies/reviews to reviews#create" do
-      expect(:post => "movies/1/reviews").to route_to("reviews#create", movie_id: '1')
+    it "routes POST movies/1/reviews to reviews#create" do
+      expect(post: "/movies/1/reviews").to route_to("reviews#create", movie_id: '1')
+    end
+
+    it "routes DELETE movies/1/reviews/1 to reviews#destroy" do
+      expect(delete: "/movies/1/reviews/1").to route_to("reviews#destroy", movie_id: "1", id: "1")
     end
 end
