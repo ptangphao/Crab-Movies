8ffd5ac9f204fd1b15862325bb9ff2b09a3ddd08
[01;31m[K"name"[m[K: "Gina Benavidez <[01;31m[K"email"[m[K: null>
2017-06-02 11:34:26 -0700

    create validations on #Movie database

--- db/migrate/20170531192122_create_movies.rb
+++ db/migrate/20170531192122_create_movies.rb
@@ -1,15 +1,15 @@
  class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      t.string :title
-      t.string :writer
-      t.string :director
-      t.integer :year
-      t.string :poster_url
-      t.integer :category_id
-      t.string :trailer_photo_url
-      t.string :trailer_url
-      t.string :plot
+      t.string :title, null: false
+      t.string :writer, null: false
+      t.string :director, null: false
+      t.integer :year, null: false
+      t.string :poster_url, null: false
+      t.integer :category_id, null: false
+      t.string :trailer_photo_url, null: false
+      t.string :trailer_url, null: false
+      t.string :plot, null: false
 
       t.timestamps null: false
     end
