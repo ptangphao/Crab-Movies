5267f389c01e074f4a8df4f7d14e8ff5b4a7d6aa
Earl Sabal <email": null>
2017-06-01 18:48:45 -0700

    update categories to validate for name and pass rspec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -13,6 +13,17 @@ describe Category do
       expect(t.macro == :has_many).to be_truthy
     end
   end
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(category).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      category.name = ''
+      expect(category).to_not be_valid
+    end
+  end
 end
 
 
