a4e11594ec98bef5f4863c7e7bf48d76f4cdc44c
[01;31m[K"name"[m[K: "Gina Benavidez <[01;31m[K"email"[m[K: null>
2017-06-02 11:34:51 -0700

    create validations on #Actors database

--- db/migrate/20170531192251_create_actors.rb
+++ db/migrate/20170531192251_create_actors.rb
@@ -1,7 +1,7 @@
 class CreateActors < ActiveRecord::Migration
   def change
     create_table :actors do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end
