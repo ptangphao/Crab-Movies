ea5ad0691a61d34eb5bce32227add5aff5da9a9e
Earl Sabal <email": null>
2017-06-01 19:03:20 -0700

    clean up text, ready for push

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 describe Actor do
   let (:actor) {Actor.new({name: "Tom Hanks"})}
+
   describe "has attributes" do
     it "has a name" do
       expect(actor.name == "Tom Hanks").to be_truthy
     end
   end
+
   describe "has associations" do
     it { is_expected.to have_many(:movies)}
     it { is_expected.to have_many(:reviews)}
     it { is_expected.to have_many(:actor_movies)}
   end
+
 end
 
 
