ce8142efba2a607881291eb9469d179f1a0c4fc0
Earl Sabal <email": null>
2017-06-01 17:31:59 -0700

    create rspec for post requests to /comments, pass rspec

--- spec/routing/comment_routing_spec.rb
+++ spec/routing/comment_routing_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Routing to comments", :type => :routing do
   it "routes POST /comments to comments#create" do
-
+    expect(:post => "/comments").to route_to("comments#create")
   end
 
   it "routes DELETE /comments/1 to comments#destroy"
