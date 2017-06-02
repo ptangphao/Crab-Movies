ed1961126c4b57ddfd3fcb3850f5efb10b1ae203
Earl Sabal <email": null>
2017-06-01 17:33:28 -0700

    create rspec for checking deletes route goes to comments#destroy, pass rspec

--- spec/routing/comment_routing_spec.rb
+++ spec/routing/comment_routing_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Routing to comments", :type => :routing do
     expect(:post => "/comments").to route_to("comments#create")
   end
 
-  it "routes DELETE /comments/1 to comments#destroy"
-    expect(:delete => "/comments").to route_to("comments#destroy", :id => "1")
+  it "routes DELETE /comments/1 to comments#destroy" do
+    expect(:delete => "/comments/1").to route_to("comments#destroy", :id => "1")
   end
 end
