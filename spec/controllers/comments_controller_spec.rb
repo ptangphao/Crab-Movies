a1993780343d03e2d28a861c7dcd66374f36869d
Earl Sabal <email": null>
2017-06-01 19:01:17 -0700

    create and pass rspec for delete#destroy for comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -9,12 +9,25 @@ describe CommentsController do
               poster_url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
               category_id: 1
               })}
+  let!(:comment) {Comment.create!({
+              user_id: 1,
+              commentable_id: movie.id,
+              commentable_type: "Movie",
+              body: "This movie is worth watching."
+              })}
   describe 'post #create' do
     it "responds with a status code 302 when valid params are passed" do
       post(:create, { comment: {user_id: 1,
                                 commentable_id: movie.id,
                                 commentable_type: "Movie",
                                 body: "laksjdf;alsdjf;asljkdfl;"}},
+                    {page_type: "Movie", page_id: movie.id})
+      expect(response).to have_http_status 302
+    end
+  end
+  describe 'DELETE #destroy' do
+    it "responds with a status code 302" do
+      delete(:destroy, { id: comment.id},
                     {page_type: "Movie", page_id: movie.id})
       expect(response).to have_http_status 302
     end
