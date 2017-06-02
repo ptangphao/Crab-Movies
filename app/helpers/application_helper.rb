4f674cb0bf4c42fd2dc446a9d1f9fe272e1a4751
Earl Sabal <email": null>
2017-06-01 18:23:25 -0700

    Implement ratings for reviews on movies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def rating(object)
     total = object.ratings.inject(0) {|sum, rating| sum += rating.rating}
-    total_stars = total / object.ratings.length
+    ratings_num = object.ratings.length
+    ratings_num > 0 ? total_stars = total / object.ratings.length : total_stars = 0
     stars = ""
     total_stars.times {stars += "☆"}
     stars
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   has_many :comments, as: :commentable
   belongs_to :user
   belongs_to :reviewable, polymorphic: :true
+  has_many :ratings, as: :rateable
 end
