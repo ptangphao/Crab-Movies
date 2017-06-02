3be13871baecabcdd1be933a44fa80b32d796029
Earl Sabal <email": null>
2017-06-01 17:26:15 -0700

    clean up formatting for routes for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :movies, only: [:show] do
     resources :reviews, only: [:create, :destroy] do
       resources :ratings, only: [:create]
     end
     resources :ratings, only: [:create]
   end
+
   resources :categories, only: [:index, :show]
+
   resources :actors, only: [:index, :show] do
     resources :reviews, only: [:create, :destroy] do
       resources :ratings, only: [:create]
     end
   end
+
   resources :comments, only: [:create, :destroy]
+
   root to:"categories#index"
 
 end
