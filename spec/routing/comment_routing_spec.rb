110a066f1bfd33253c826b631ec9a68955e8c3f9
Patrick Tangphao <email": null>
2017-06-02 14:37:48 -0700

    update comment routing specs for routing within reviews within movies, pass rspec

--- spec/routing/comment_routing_spec.rb
+++ spec/routing/comment_routing_spec.rb
@@ -1,17 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe "Routing to comments", :type => :routing do
-#   context "Routes" do
-#     it "routes POST /comments to comments#create" do
-#       expect(:post => "/comments").to route_to("comments#create")
-#     end
-
-#     it "routes DELETE /comments/1 to comments#destroy" do
-#       expect(:delete => "/comments/1").to route_to("comments#destroy", :id => "1")
-#     end
-#   end
-# end
-
 require 'rails_helper'
 
 RSpec.describe "Routing to comments", :type => :routing do
@@ -24,4 +10,14 @@ RSpec.describe "Routing to comments", :type => :routing do
       expect(delete: "/movies/1/comments/1").to route_to("comments#destroy", movie_id: "1", id: "1")
     end
   end
+  describe "Routing within reviews within movies" do
+    it "routes POST movies/1/reviews/1/comments to comments#create" do
+      expect(post: "/movies/1/reviews/1/comments").to route_to("comments#create", movie_id: '1', review_id: '1')
+    end
+
+    it "routes DELETE movies/1/reviews/1/comments/1 to comments#destroy" do
+      expect(delete: "/movies/1/reviews/1/comments/1").to route_to("comments#destroy", movie_id: "1", review_id: "1", id: "1")
+    end
+  end
+
 end
